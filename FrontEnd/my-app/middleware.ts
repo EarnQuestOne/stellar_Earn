25c9285155811b3cae16e2bd46a85decccb5caca
Opulence Chuks <chukwudaluchiemerie@gmail.com>
2026-07-09 20:09:14 +0100

    style: format middleware.ts to conform with Prettier line length standard

--- FrontEnd/my-app/middleware.ts
+++ FrontEnd/my-app/middleware.ts
@@ -34,7 +34,10 @@ export function middleware(request: NextRequest) {
     if (config.headers && Array.isArray(config.headers)) {
       config.headers.forEach((header) => {
         if (header.key === 'Content-Security-Policy') {
-          const value = header.value.replace(/'unsafe-inline'/g, `'nonce-${nonce}'`);
+          const value = header.value.replace(
+            /'unsafe-inline'/g,
+            `'nonce-${nonce}'`
+          );
           response.headers.set(headerKey, value);
         } else {
           response.headers.set(header.key, header.value);
